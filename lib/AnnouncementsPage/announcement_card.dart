ef300af380ad2aefe1177bb3022dc09dd56f8d02
Ishita <ishita11.ishu@gmail.com>
2023-01-29 01:10:52 +0530

    added announcement card

--- lib/AnnouncementsPage/announcement_card.dart
+++ lib/AnnouncementsPage/announcement_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:makeathon5_app/AnnouncementsPage/announcement.dart';
+import 'package:flutter/material.dart';
+import 'announcement.dart';
 
 class AnnouncementCard extends StatelessWidget {
   Announcement? announcement;
@@ -7,7 +8,43 @@ class AnnouncementCard extends StatelessWidget {
   AnnouncementCard(this.announcement);
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Flexible(
+        flex: 12,
+        child: Card(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+            child: ListTile(
+                title: Row(
+                  children: [
+                    Text(
+                      '5:30 PM',
+                      style: TextStyle(
+                          fontFamily: 'Inter', fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    Text(
+                      '28 Jan',
+                      style: TextStyle(
+                          fontFamily: 'Inter', fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                subtitle: Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Inter',
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                )),
+          ),
+          elevation: 4,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        ));
   }
 }
